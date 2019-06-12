e062ff5e0cf60c7c649009f0026bf98a3bf6d5f0
Charley Young <post.megaa@gmail.com>
2019-06-12 15:02:03 +0800

    Revert "adjust file"
    This reverts commit 52707c72e8bf450f8d2dd631fbda5c976f810ef9.

---
+++ webTSdemo/webTSdemo/src/App.jsx
@@ -0,0 +1,28 @@
+﻿import React, { Component } from "react";
+import { Route, Switch, Redirect, BrowserRouter } from "react-router-dom";
+import "./App.css";
+import NavBar from "./components/NavBar";
+import Footer from "./components/Footer";
+
+import Master_Detail from "./components/Master_Detail";
+import Blank3 from "./components/Blank3";
+//TODO Web Template Studio: Add routes for your new pages here.
+class App extends Component {
+  render() {
+    return (
+      <React.Fragment>
+        <NavBar />
+        <BrowserRouter>
+          <Switch>
+            <Redirect exact path = "/" to = "/Master_Detail" />
+            <Route path = "/Master_Detail" component = { Master_Detail } />
+            <Route path = "/Blank3" component = { Blank3 } />
+          </Switch>
+        </BrowserRouter>
+        <Footer />
+      </React.Fragment>
+    );
+  }
+}
+
+export default App;
