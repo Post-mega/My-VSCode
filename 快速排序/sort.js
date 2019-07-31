6d2b2a84b42cb19efc5d383f63aec83e22aacffd
Charley Young <post.megaa@gmail.com>
2019-07-31 22:30:47 +0800

    Create sort.js

---
+++ 快速排序/sort.js
@@ -0,0 +1,14 @@
+/**
+ *
+ *
+ * @param {*} arr
+ * @returns
+ */
+function quickSort(arr) {
+    if(arr.length<=1){
+        return arr;
+    }
+    var less = new Array();
+    var greater = new Array();
+    var pivotIndex = Math
+}
