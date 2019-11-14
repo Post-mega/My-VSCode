af8dc91e4dc7d79d2ad39ec8aeda5be4bab2c0cc
yangchenyuan <yangchenyuan.97@bytedance.com>
2019-11-14 11:36:24 +0800

    update files

--- test.js
+++ test.js
@@ -1,5 +1,14 @@
-var foo = 1;
-(function foo() {
-    foo = 10;
-    console.log(foo)
-})()
+// let _  = require('lodash')
+// let curry = _.curry;
+
+// // let match = curry(function(what, str) {
+// //     return str.match(what)
+// // })
+
+// let match = curry((what, str) => console.log(str.match(what)))
+
+// match(/\s+/g, 'hello world')
+
+// let hasSpaces = match(/\s+/g)
+
+// hasSpaces('www ss')
